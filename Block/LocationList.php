80da102f9c52453a81303bc4f7b7c3737c8d4224
diepxuan <ductn@diepxuan.com>
2025-04-03 16:14:49 +0000

    Add Topmenu plugin to enhance category tree management in storefront menu

---
+++ Block/LocationList.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2024-09-07 20:45:46
+ */
+
+namespace Diepxuan\GoogleMaps\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class LocationList extends Template implements BlockInterface {}
